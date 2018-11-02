a4109d53f95632cb4b0f9f5b6baaa3fa39873595
Artur <Artur@ARTUR-PC>
2018-11-02 14:24:34 -0200

    Implementacao

---
+++ BackendCSharpOAuth.Repositorio/Base/RepositorioBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCSharpOAuth.Repositorio.Base
+{
+    public class RepositorioBase
+    {
+        private BancoContext _entidades;
+        protected BancoContext entidades
+        {
+            get
+            {
+                if (_entidades == null)
+                {
+                    _entidades = new BancoContext();
+                }
+                return _entidades;
+            }
+        }
+    }
+}
--- BackendCSharpOAuth.Repositorio/RepCarros.cs
+++ BackendCSharpOAuth.Repositorio/RepCarros.cs
@@ -1,6 +1,7 @@
 ﻿using BackendCSharpOAuth.Dominio;
 using BackendCSharpOAuth.Infra;
 using BackendCSharpOAuth.Infra.DTOs;
+using BackendCSharpOAuth.Repositorio.Base;
 using Microsoft.Practices.ServiceLocation;
 using System;
 using System.Collections.Generic;
@@ -10,21 +11,8 @@ using System.Threading.Tasks;
 
 namespace BackendCSharpOAuth.Repositorio
 {
-    public class RepCarros : IRepCarros
+    public class RepCarros : RepositorioBase, IRepCarros
     {
-        private BancoContext _entidades;
-        protected BancoContext entidades
-        {
-            get
-            {
-                if (_entidades == null)
-                {
-                    _entidades = new BancoContext();
-                }
-                return _entidades;
-            }
-        }
-
         public List<Carros> Listar(QueryPaginacaoDTO dto)
         {
             return entidades.Carros.OrderBy(x => x.Id).Skip((dto.Page - 1) * dto.Limit).Take(dto.Limit).ToList();
