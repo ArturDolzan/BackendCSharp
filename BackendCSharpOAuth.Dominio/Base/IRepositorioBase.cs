80978516133f7f7daed5dc3ce592e3ea61a46779
Artur Dolzan Neto <arturdolzan@gmail.com>
2018-11-05 12:55:05 -0200

    inserindo

---
+++ BackendCSharpOAuth.Dominio/Base/IRepositorioBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCSharpOAuth.Dominio.Base
+{
+    public interface IRepositorioBase<TEntidade> : IUnidadeDeTrabalho<TEntidade> where TEntidade: class
+    {        
+        IQueryable<TEntidade> Recuperar();
+        IQueryable<TEntidade> Recuperar(string includes);
+    }
+}
---
+++ BackendCSharpOAuth.Dominio/IUnidadeDeTrabalho.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCSharpOAuth.Dominio
+{
+    public interface IUnidadeDeTrabalho<TEntidade> where TEntidade : class
+    {
+        DbSet<TEntidade> Entidade
+        {
+            get;
+            set;
+        }
+
+        int PersistirTransacao();
+        void RejeitarTransacao();
+        DbContextTransaction CriarTransacaoEmEscopo();
+    }
+}
