8db56f07f9a51aebbb14df23a60b2f61ea19a572
Artur <Artur@ARTUR-PC>
2018-11-05 23:05:24 -0200

    Implementacao

---
+++ BackendCSharpOAuth.Dominio/Base/IServicoBase.cs
@@ -0,0 +1,15 @@
+﻿using BackendCSharpOAuth.Infra;
+using BackendCSharpOAuth.Infra.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCSharpOAuth.Dominio.Base
+{
+    public interface IServicoBase<TEntidade> where TEntidade : IdentificadorBase
+    {
+        List<TEntidade> Listar(QueryPaginacaoDTO dto);
+    }
+}
---
+++ BackendCSharpOAuth.Dominio/Base/ServicoBase.cs
@@ -0,0 +1,26 @@
+﻿using BackendCSharpOAuth.Infra;
+using BackendCSharpOAuth.Infra.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendCSharpOAuth.Dominio.Base
+{
+    public class ServicoBase<TEntidade> : IServicoBase<TEntidade> where TEntidade: IdentificadorBase
+    {
+        public IRepositorioBase<TEntidade> _repositorio;
+
+        public ServicoBase()
+        {
+            //_repositorio = repositorio;
+        }
+
+        public List<TEntidade> Listar(QueryPaginacaoDTO dto)
+        {
+            return _repositorio.Recuperar().ToList();
+        }
+        
+    }
+}
